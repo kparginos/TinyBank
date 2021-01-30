cbfe474833af6eec4815243ce1f41e9864097de8
kparginos <kparginos@yahoo.gr>
2021-01-30 16:15:30 +0200

    Add TransactionStateType to Transaciotn Table.

--- app/TinyBank/Migrations/20210130141313_Add_TransactionStateType_Column.Designer.cs
+++ app/TinyBank/Migrations/20210130141313_Add_TransactionStateType_Column.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TinyBank.Core.Data;
 
 namespace TinyBank.Migrations
 {
     [DbContext(typeof(TinyBankDBContext))]
-    partial class TinyBankDBContextModelSnapshot : ModelSnapshot
+    [Migration("20210130141313_Add_TransactionStateType_Column")]
+    partial class Add_TransactionStateType_Column
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
