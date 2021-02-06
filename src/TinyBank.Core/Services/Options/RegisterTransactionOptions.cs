cd01d00711fccbf2deb5bff538fe686870337076
kparginos <kparginos@yahoo.gr>
2021-02-06 20:02:26 +0200

    Add options to be used by the services.

---
+++ src/TinyBank.Core/Services/Options/RegisterTransactionOptions.cs
@@ -0,0 +1,12 @@
+﻿using TinyBank.Core.Model.Types;
+
+namespace TinyBank.Core.Services.Options
+{
+    public class RegisterTransactionOptions
+    {
+        public string TransDescr { get; set; }
+        public TransactionType Type { get; set; }
+        public decimal Amount { get; set; }
+        public TransactionStateType State { get; set; } = TransactionStateType.Pending;
+    }
+}
