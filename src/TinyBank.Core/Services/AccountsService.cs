73aa3c7193874cc925b1a1cb88de79de294d747d
kparginos <kparginos@yahoo.gr>
2021-02-06 20:02:59 +0200

    Add Customer Service Add Account Service Add Transaciton Service

---
+++ src/TinyBank.Core/Services/AccountsService.cs
@@ -0,0 +1,57 @@
+﻿using System.Linq;
+
+using TinyBank.Core.Data;
+using TinyBank.Core.Model;
+using TinyBank.Core.Services.Interfaces;
+using TinyBank.Core.Services.Options;
+
+namespace TinyBank.Core.Services
+{
+    public class AccountsService : IAccountsService
+    {
+        private readonly TinyBankDBContext _dBContext;
+        private ICustomerService _customer;
+
+        public AccountsService(TinyBankDBContext dBContext, ICustomerService customer)
+        {
+            _dBContext = dBContext;
+            _customer = customer;
+        }
+
+        public Accounts GetAccountbyID(int accountID)
+        {
+            return _dBContext.Set<Accounts>()
+                .Where(a => a.AccountsId == accountID)
+                .SingleOrDefault();
+        }
+
+        public Accounts Register(int customerID, RegisterAccountOptions options)
+        {
+            if (string.IsNullOrWhiteSpace(options.AccountNumber))
+                return null;
+            
+            if (string.IsNullOrWhiteSpace(options.AccountDescr))
+                return null;
+            
+            if (string.IsNullOrWhiteSpace(options.Currency))
+                return null;
+
+            var customer = _customer.GetCustomerbyID(customerID);
+            if (customer == null)
+                return null;
+
+            var account = new Accounts()
+            {
+                AccountDescription = options.AccountDescr,
+                AccountNumber = options.AccountNumber,
+                Currency = options.Currency,
+            };
+
+            customer.Accounts.Add(account);
+
+            _dBContext.SaveChanges();
+
+            return account;
+        }
+    }
+}
---
+++ src/TinyBank.Core/Services/CustomerService.cs
@@ -0,0 +1,55 @@
+﻿using System.Linq;
+using TinyBank.Core.Data;
+using TinyBank.Core.Model;
+using TinyBank.Core.Services.Interfaces;
+using TinyBank.Core.Services.Options;
+
+namespace TinyBank.Core.Services
+{
+    public class CustomerService : ICustomerService
+    {
+        long vatNumber = 0;
+        private readonly TinyBankDBContext _dbContext;
+
+        public CustomerService(TinyBankDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public Customer Register(RegisterCustomerOptions options)
+        {
+            if (string.IsNullOrWhiteSpace(options.Name))
+                return null;
+
+            if (string.IsNullOrWhiteSpace(options.Name))
+                return null;
+
+            if (options.VATNumber.Length != 9)
+                return null;
+
+            if (!long.TryParse(options.VATNumber, out vatNumber))
+                return null;
+
+            var customer = new Customer()
+            {
+                Name = options.Name,
+                SureName = options.SureName,
+                VatNumber = options.VATNumber,
+                CustBankID = options.CustomerBankID,
+                CustType = options.CustType,
+                Address = options.Address
+            };
+
+            _dbContext.Add<Customer>(customer);
+            _dbContext.SaveChanges();
+
+            return customer;
+        }
+
+        public Customer GetCustomerbyID(int customerID)
+        {
+            return _dbContext.Set<Customer>()
+                .Where(c => c.CustomerId == customerID)
+                .SingleOrDefault();
+        }
+    }
+}
