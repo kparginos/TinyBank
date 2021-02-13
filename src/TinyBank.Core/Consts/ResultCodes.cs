4fd42aed34e4b13fa9883c522ddc4f3734e93c5e
kparginos <kparginos@yahoo.gr>
2021-02-13 19:21:41 +0200

    Add generic Result Codes.

---
+++ src/TinyBank.Core/Consts/ResultCodes.cs
@@ -0,0 +1,10 @@
+﻿namespace TinyBank.Core.Consts
+{
+    public static class ResultCodes
+    {
+        public const int Success = 200;
+        public const int BadRequest = 400;
+        public const int NotFound = 404;
+        public const int InternalServerError = 500;
+    }
+}
