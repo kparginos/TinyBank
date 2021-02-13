f20c80c81a67aa4631d4b6e0a44149a3bc167531
kparginos <kparginos@yahoo.gr>
2021-02-13 19:21:12 +0200

    Rename column Active to State for table Accounts

--- app/TinyBank.Migrations/Migrations/20210213160332_Rename_Account_Column_Active_to_State.Designer.cs
+++ app/TinyBank.Migrations/Migrations/20210213160332_Rename_Account_Column_Active_to_State.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TinyBank.Core.Data;
 
 namespace TinyBank.Migrations
 {
     [DbContext(typeof(TinyBankDBContext))]
-    partial class TinyBankDBContextModelSnapshot : ModelSnapshot
+    [Migration("20210213160332_Rename_Account_Column_Active_to_State")]
+    partial class Rename_Account_Column_Active_to_State
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ app/TinyBank.Migrations/Migrations/20210213160332_Rename_Account_Column_Active_to_State.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TinyBank.Migrations
+{
+    public partial class Rename_Account_Column_Active_to_State : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Active",
+                table: "Accounts");
+
+            migrationBuilder.AddColumn<int>(
+                name: "State",
+                table: "Accounts",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "State",
+                table: "Accounts");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Active",
+                table: "Accounts",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
