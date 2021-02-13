36e8e23e1b3a82298fdc12eda9f792effd21a780
kparginos <kparginos@yahoo.gr>
2021-02-13 19:18:49 +0200

    Add Result class.

---
+++ src/TinyBank.Core/Result.cs
@@ -0,0 +1,10 @@
+﻿
+namespace TinyBank.Core
+{
+    public class Result<T>
+    {
+        public int Code { get; set; }
+        public string Message { get; set; }
+        public T Data { get; set; }
+    }
+}
