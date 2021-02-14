93f2d79e148e113746af02a290dd883be6fe48fe
kparginos <kparginos@yahoo.gr>
2021-02-14 13:55:35 +0200

    Refactoring for Result and ResultList classes

---
+++ src/TinyBank.Core/Services/Results/Result.cs
@@ -0,0 +1,9 @@
+﻿namespace TinyBank.Core.Services.Results
+{
+    public class Result<T>
+    {
+        public int Code { get; set; }
+        public string Message { get; set; }
+        public T Data { get; set; }
+    }
+}
---
+++ src/TinyBank.Core/Services/Results/ResultList.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace TinyBank.Core.Services.Results
+{
+    public class ResultList<T>
+    {
+        public int Code { get; set; }
+        public string Message { get; set; }
+        public List<T> Data { get; set; }
+    }
+}
