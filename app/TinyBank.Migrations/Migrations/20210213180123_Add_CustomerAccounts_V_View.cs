0a5f15d49e7212b3d190d393fb367e2e23916a8f
kparginos <kparginos@yahoo.gr>
2021-02-14 13:45:36 +0200

    Add CustomerAccountsView to migrations

---
+++ app/TinyBank.Migrations/Migrations/20210213180123_Add_CustomerAccounts_V_View.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TinyBank.Migrations
+{
+    public partial class Add_CustomerAccounts_V_View : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Create Or Alter View CustomerAccounts_V As " +
+                                    "Select  C.CustomerId, C.Name, C.SureName, C.Address, C.CustType, C.CustBankID, C.VatNumber, C.Created As CustCreateDT, C.Active," +
+                                    "A.AccountsId, A.AccountNumber, A.AccountDescription, A.State, A.Balance, A.Currency, A.Created AccountCreateDate " +
+                                "From Customer C " +
+                                "   Left Join Accounts A On A.CustomerId = C.CustomerId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP VIEW [dbo].[CustomerAccounts_V]");
+        }
+    }
+}
