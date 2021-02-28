77415633eae1df20993a783167ad5847b5565663
kparginos <kparginos@yahoo.gr>
2021-02-28 18:53:04 +0200

    Add Transactions Controller

---
+++ app/TinyBank.API/Controllers/TransactionsController.cs
@@ -0,0 +1,46 @@
+﻿using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+
+using TinyBank.API.Controllers.Requests;
+using TinyBank.Core.Consts;
+using TinyBank.Core.Services.Interfaces;
+
+namespace TinyBank.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TransactionsController : Controller
+    {
+        private readonly ITransactionService _transaction;
+        private readonly IAccountsService _account;
+
+        public TransactionsController(
+            ITransactionService transaction,
+            IAccountsService account)
+        {
+            _transaction = transaction;
+            _account = account;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(
+            [FromBody] TransactionRegisterRequest request)
+        {
+            var accountResult = await _account.GetAccountbyCustomerIDAsync(request.CustomerID, request.AccountID);
+
+            if (accountResult.Code == ResultCodes.Success)
+            {
+                var result = await _transaction.RegisterAsync(
+                    accountResult.Data.AccountsId,
+                    request.Options);
+
+                return Json(result);
+            }
+            else
+            {
+                return Json(accountResult);
+            }
+        }
+    }
+}
