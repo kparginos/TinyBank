f56713887efabc020db526e683bb4d362b5bba89
kparginos <kparginos@yahoo.gr>
2021-03-16 10:51:43 +0200

    Add AuditInfo entity

---
+++ src/TinyBank.Model/AuditInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TinyBank.Model
+{
+    public class AuditInfo
+    {
+        public DateTimeOffset Created { get; set; }
+        public DateTimeOffset? Updated { get; set; }
+
+        public AuditInfo()
+        {
+            Created = DateTime.Now;
+            Updated = Created;
+        }
+
+    }
+}
