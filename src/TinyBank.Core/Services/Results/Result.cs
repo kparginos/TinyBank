0f851e62ff7a6375f187d5abf6deabecfae39c1f
kparginos <kparginos@yahoo.gr>
2021-03-19 10:54:39 +0200

    Add IsSuccess method.

--- src/TinyBank.Core/Services/Results/Result.cs
+++ src/TinyBank.Core/Services/Results/Result.cs
@@ -1,9 +1,16 @@
-﻿namespace TinyBank.Core.Services.Results
+﻿using TinyBank.Core.Consts;
+
+namespace TinyBank.Core.Services.Results
 {
     public class Result<T>
     {
         public int Code { get; set; }
         public string Message { get; set; }
         public T Data { get; set; }
+
+        public bool IsSuccess()
+        {
+            return Code == ResultCodes.Success;
+        }
     }
 }
