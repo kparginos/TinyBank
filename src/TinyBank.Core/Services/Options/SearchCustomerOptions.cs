d356a5f917d814fcc27b579d31c9ac88b3736f7d
kparginos <kparginos@yahoo.gr>
2021-03-23 09:07:35 +0200

    Add SearchCustomerOptions

---
+++ src/TinyBank.Core/Services/Options/SearchCustomerOptions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TinyBank.Core.Services.Options
+{
+    public class SearchCustomerOptions
+    {
+        public string VAtNumber { get; set; }
+        public int? CustomerId { get; set; }
+        public string CustBankId { get; set; }
+        public int? MaxResults{ get; set; }
+        public int? Skip { get; set; }
+        public List<string> CountryCodes { get; set; }
+
+        public SearchCustomerOptions()
+        {
+            CountryCodes = new List<string>();
+        }
+    }
+}
